d96217f67266afb1f62c7175b0b4965dac62aed1
syedomair <syedkhalidomair@gmail.com>
2016-03-26 10:13:34 +0500

    added data dump

--- projectDB.sql
+++ projectDB.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: project1
 -- ------------------------------------------------------
--- Server version	5.5.44-0ubuntu0.14.04.1
+-- Server version	5.5.47-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,14 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `project1`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `project1` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `project1`;
 
 --
 -- Table structure for table `catalog`
@@ -61,7 +53,7 @@ CREATE TABLE `category` (
   PRIMARY KEY (`id`),
   KEY `IDX_64C19C1CC3C66FC` (`catalog_id`),
   CONSTRAINT `FK_64C19C1CC3C66FC` FOREIGN KEY (`catalog_id`) REFERENCES `catalog` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +62,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,1,'Men'),(2,1,'Women');
+INSERT INTO `category` VALUES (1,1,'Men'),(2,1,'Women'),(3,1,'Children');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,6 +96,37 @@ LOCK TABLES `product` WRITE;
 INSERT INTO `product` VALUES (1,1,'Men Shirts Dress Khaki','EW239','46.99','The description for Men Shirt Dress Khaki','mens_shirts_dresscas_Dkhaki.gif'),(2,1,'Men Pants Dress','FD320','45.99','The description for Men Pants Dress','mens_pants_dress.gif'),(3,1,'Men Pants Cords','HG875','10.99','The description for Men Pants Cords','mens_pants_cords.gif'),(4,1,'Men Shirts Dress Stone','CY543','68.99','The description for Men Shirts Dress Stone','mens_shirts_dress_stone.gif'),(5,1,'Men Shirts Dress Stone II','TY332','67.99','The description for Men Shirts Dress Stone II','mens_shirts_dresscas_stone.gif'),(6,2,'Women Sleepwear','BC198','21.99','The description for Women Sleepwear','womens_sleepwear_short_Iblue.gif'),(7,2,'Women Dress','AY723','78.99','The description for Women Dress','womens_dress_pink_Db.gif'),(8,2,'Women Activewear Soccer','PO193','23.99','The description for Women Activewear Soccer','womens_activewear_soccer_Ob.gif'),(9,2,'Women Activewear 1Piece','CV389','32.99','The description for Women Activewear 1Piece','womens_activewear_1piece_in.gif'),(10,2,'Women Accessories Handbag','SBC163','15.99','The description for Women Accessories Handbag','womens_accessories_handbag.gif'),(11,2,'Women Accessories Belt Nav','AU0023','11.99','The description for Women Accessories Belt Nav','womens_accessories_belt_nav.gif'),(12,2,'Women Accessories Belt Brown','BC003','10.99','The discription for Women Accessories Belt Brown','womens_accessories_belt_brw.gif');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `given_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `family_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `email_verified` tinyint(1) DEFAULT NULL,
+  `password` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (40,'omair1@gmail.com','Syed','Omair','2016-03-23 10:05:21','2016-03-23 10:05:21',NULL,'1fHh20BXyMalo','1fju6dt0wl1csgcogwkgsc84w');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -114,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-24 21:07:58
+-- Dump completed on 2016-03-26 10:10:57
